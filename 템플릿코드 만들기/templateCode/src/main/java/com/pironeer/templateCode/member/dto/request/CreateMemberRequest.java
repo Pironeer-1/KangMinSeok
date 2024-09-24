fc95137838f02c01e12b06dc6ae22526a9986b42
minseok kang <alstjrrkd201@gmail.com>
2024-09-24 10:58:48 +0900

    member Response 클래스 ì생성

--- 템플릿코드 만들기/templateCode/src/main/java/com/pironeer/templateCode/member/dto/request/CreateMemberRequest.java
+++ 템플릿코드 만들기/templateCode/src/main/java/com/pironeer/templateCode/member/dto/request/CreateMemberRequest.java
@@ -2,7 +2,4 @@ package com.pironeer.templateCode.member.dto.request;
 
 public record CreateMemberRequest(String MemberId, String password, String name) {
 
-    public CreateMemberRequest of(String MemberId, String password, String name) {
-        return new CreateMemberRequest(MemberId, password, name);
-    }
 }
---
+++ 템플릿코드 만들기/templateCode/src/main/java/com/pironeer/templateCode/member/dto/response/CreateMemberResponse.java
@@ -0,0 +1,10 @@
+package com.pironeer.templateCode.member.dto.response;
+
+import com.pironeer.templateCode.member.dto.request.CreateMemberRequest;
+
+public record CreateMemberResponse(String memberId, String password, String name) {
+    public static CreateMemberRequest of(String MemberId, String password, String name) {
+        return new CreateMemberRequest(MemberId, password, name);
+    }
+}
+s
